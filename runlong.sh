40683f2971084eb1a72667d14c347dab4717c611
RF216 <157068144+RF216@users.noreply.github.com>
2026-06-23 11:59:51 +0800

    Update input parameters for pmemd.cuda command

--- runlong.sh
+++ runlong.sh
@@ -2,7 +2,7 @@
 
 export PATH=/data/tools/pmemd24/bin:$PATH
 
-pmemd.cuda -O -i prodlong.in -p abl_ligand.prmtop -c step9.rst7 -o abl_lig.out -x abl_lig.nc -r abl_lig.rst7 -inf abl_lig.mdinfo
+pmemd.cuda -O -i prodlong.in -p ablhmr.prmtop -c step9.rst7 -o abl_lig.out -x abl_lig.nc -r abl_lig.rst7 -inf abl_lig.mdinfo
 
 
 exit 0
